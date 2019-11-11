375e839dd4c426dde6814017d9dfe62aab2f1c4d
Nefedov Denys <den.nefedov@gmail.com>
2019-11-11 15:12:34 +0200

    add new page (about.html), simple corrections in css

--- maticpic.loc/js/count.js
+++ maticpic.loc/js/count.js
@@ -1,19 +1,39 @@
+var currentNumber = $('.projects').text();
+
+$({numberValue: currentNumber}).animate({numberValue: 476}, {
+    duration: 5000,
+    easing: 'linear',
+    step: function() { 
+        $('.projects').text(Math.ceil(this.numberValue)); 
+    }
+});
+
+var currentNumber = $('.hours').text();
+
+$({numberValue: currentNumber}).animate({numberValue: 1843}, {
+    duration: 5000,
+    easing: 'linear',
+    step: function() { 
+        $('.hours').text(Math.ceil(this.numberValue)); 
+    }
+});
+
+var currentNumber = $('.feedbacks').text();
+
+$({numberValue: currentNumber}).animate({numberValue: 516}, {
+    duration: 5000,
+    easing: 'linear',
+    step: function() { 
+        $('.feedbacks').text(Math.ceil(this.numberValue)); 
+    }
+});
+
 var currentNumber = $('.clients').text();
 
-$({numberValue: currentNumber}).animate({numberValue: 271}, {
+$({numberValue: currentNumber}).animate({numberValue: 675}, {
     duration: 5000,
     easing: 'linear',
     step: function() { 
         $('.clients').text(Math.ceil(this.numberValue)); 
-    }
-});
-
-var currentNumber = $('.works').text();
-
-$({numberValue: currentNumber}).animate({numberValue: 984}, {
-    duration: 5000,
-    easing: 'linear',
-    step: function() { 
-        $('.works').text(Math.ceil(this.numberValue)); 
     }
 });
