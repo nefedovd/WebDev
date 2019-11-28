5048a37d84623c8176687d361ffb7fadea274c3e
Nefedov Denys <den.nefedov@gmail.com>
2019-11-28 18:36:27 +0200

    just update some files
    no change

--- maticpic.loc/js/main.js
+++ maticpic.loc/js/main.js
@@ -46,6 +46,35 @@ $.each($target,function(i,t){
      setTimeout(function(){ $this.show('normal'); },i*hold);
 });
 
+//
+// Modal About Page
+//
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 
 //
 //Slider Script
@@ -66,17 +95,21 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
+  var dots = document.getElementsByClassName("SlideThumb");
   var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].className.replace("displayNone");
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" S-active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].className.replace("displayBlock");
   dots[slideIndex-1].className += " S-active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
